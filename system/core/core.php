82fc2eb50eee1ca670f3272fec557d7b68b49aa0
SCH <sch@xcgzs.cn>
2021-05-15 19:49:06 +0800

    更新核心代码，增加虚拟地址

--- system/core/core.php
+++ system/core/core.php
@@ -1,2 +1,29 @@
 <?php
+
 die("<h1>欢迎访问20J1网站</h1>");
+
+
+
+function getPath()//获取当前虚拟地址
+{
+    $a = "/";//未加参数的情况
+    if (isset($_SERVER['QUERY_STRING'])) {//指定地址参数时
+        $a = $_SERVER['QUERY_STRING'];//读取请求参数的内容
+    }
+    $b = stripos($a, "&");//获取&符号第一次出现位置
+    if ($b != "") {
+        $c = substr($a, 0, $b);//获取第一参数
+    } else {
+        $c = $a;//未加参数时返回/
+    }
+    if ($c == "") {
+        $c = "/";//参数为空时返回/
+    }
+    if (substr($c, strlen($c) - 1, 1) == "/") {
+        $c = substr($c, 0, strlen($c) - 1);//若地址最后存在一个/，将删除这个/
+    }
+    if ($c == "") {
+        $c = "/";//地址未空时变为/
+    }
+    return $c;//返回地址
+}
