bd4056091fbc0c3a558783f854f2e45c43584d87
SCH <sch@xcgzs.cn>
2021-05-15 22:44:36 +0800

    更新核心代码

--- system/core/core.php
+++ system/core/core.php
@@ -1,11 +1,23 @@
 <?php
-include("system/core/define.php");
-loadHead("欢迎");
-die("<h1>欢迎访问20J1网站</h1>");
 
+initialize();
+switch (getPath()){
+    case "/":
+        get::index();
+        break;
+    default:
+        break;
 
+}
 
-function getPath()//获取当前虚拟地址
+//初始化
+function initialize(){
+    include("system/core/define.php");
+    loadComponent("get");//加载get请求模块
+}
+
+//获取当前虚拟地址
+function getPath()
 {
     $a = "/";//未加参数的情况
     if (isset($_SERVER['QUERY_STRING'])) {//指定地址参数时
@@ -28,8 +40,45 @@ function getPath()//获取当前虚拟地址
     }
     return $c;//返回地址
 }
-function loadHead($title){
-    $value = file_get_contents(HTML_PATH."head.html");
-    $value = str_replace("{TITLE}",$title,$value);
+
+//加载模块
+function loadComponent($name){
+    include(CORE_PATH.$name.".php");
+}
+
+//加载头部文件
+function loadHead(string $title,array $extraFile=null):void
+{
+    $value = file_get_contents(HTML_PATH . "head.html");//读取头部文件
+    $value = str_replace("{TITLE}", $title, $value);//设置标题
+    $extraFileText = "";
+    if ($extraFile){
+        if (isset($extraFile["js"])){
+            $js = $extraFile["js"];
+            for ($i = 0;$i<count($js);$i++){
+                $extraFileText.="<script src='".JS_PATH.$js[$i].".js'></script>";
+            }
+        }
+        if (isset($extraFile["css"])){
+            $css = $extraFile["css"];
+            for ($i = 0;$i<count($css);$i++){
+                $extraFileText.="<link href='".CSS_PATH.$css[$i].".css' rel='stylesheet' type='text/css'>";
+            }
+        }
+    }
+    $value = str_replace("{extraFile}",$extraFileText,$value);
+    echo $value;
+}
+
+//加载HTML文件
+function loadHTML($name, $args = null)
+{
+    $value = file_get_contents(HTML_PATH . $name . ".html");
+    if ($args) {
+        for ($i = 0; $i < count($args); $i++) {
+            $key = array_keys($args)[$i];
+            str_replace("{" . $key . "}", $args[$key], $value);
+        }
+    }
     echo $value;
 }
