01f42f159a1b6fc029431b22f7e6a1f6a348b904
SCH <sch@xcgzs.cn>
2021-05-16 08:07:56 +0800

    更新GET模块，增加index页面图片引用

--- system/core/get.php
+++ system/core/get.php
@@ -3,7 +3,7 @@
 class get
 {
     public static function index():void{
-        loadHead("欢迎来到20计1",["js"=>["index"],"css"=>["index"]]);
-        loadHTML("index");
+        loadHead("欢迎来到20计1",["js"=>["index","index2"],"css"=>["index"]]);
+        loadHTML("index",["IMG_j1logo_jpg"=>IMG_PATH."j1logo.jpg"]);
     }
 }
