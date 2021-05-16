5e67c70d650f730435cfdd65976c9b7808aa82bc
SCH <sch@xcgzs.cn>
2021-05-16 10:49:33 +0800

    更新GET模块，取消外部文件参数

--- system/core/get.php
+++ system/core/get.php
@@ -3,7 +3,7 @@
 class get
 {
     public static function index():void{
-        loadHead("欢迎来到20计1",["js"=>["index","index2"],"css"=>["index"]]);
-        loadHTML("index",["IMG_j1logo_jpg"=>IMG_PATH."j1logo.jpg"]);
+        loadHead("欢迎来到20计1",["js"=>["index","index2"],"css"=>[["index","media=\"screen\""]]]);
+        loadHTML("index");
     }
 }
