51afb87f1c3d93debb47d7d7611b939f0b5d2099
SCH <sch@xcgzs.cn>
2021-05-23 11:09:49 +0800

    增加文件模块

---
+++ system/core/fileTool.php
@@ -0,0 +1,19 @@
+<?php
+
+class fileTool
+{
+    public static function createDir(string $path):bool
+    {
+        $aimUrl = str_replace('', '/', $path);
+        $aimDir = '';
+        $arr = explode('/', $aimUrl);
+        $result = true;
+        foreach ($arr as $str) {
+            $aimDir .= $str . '/';
+            if (!file_exists($aimDir)) {
+                $result = mkdir($aimDir);
+            }
+        }
+        return $result;
+    }
+}
