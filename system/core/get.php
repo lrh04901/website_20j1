4cda5b9f1a51390dd2f66d43d5dd16df039907bd
lrhhz <2372724252@qq.com>
2021-05-23 17:54:01 +0800

    "优化"

--- system/core/get.php
+++ system/core/get.php
@@ -4,14 +4,14 @@ class get
 {
     public static function index(){
         loadHead(L["INDEX_TITLE"],["css"=>[["index","media=\"screen\""]]]);
-        loadBody("index");
+        loadHtml("index");
     }
     public static function update(){
         loadHead("更新站点",["css"=>["zui.min","zui.uploader.min"],"js"=>["zui.uploader.min"]]);
-        loadBody("update");
+        loadHtml("update");
     }
     public static function classIntroduce(){
         loadHead("胡杨班",["css"=>[["classIntroduce","media=\"screen\""]]]);
-        loadBody("classIntroduce");
+        loadHtml("classIntroduce");
     }
 }
