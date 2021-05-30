cefd8544d16520a66f106aeebf6d3f7b086d344e
SCH <sch@xcgzs.cn>
2021-05-30 21:02:13 +0800

    更新post模块，修改部分代码

--- system/core/post.php
+++ system/core/post.php
@@ -14,8 +14,9 @@ class post
         $log.="POST变量：".json_encode($_POST)."\r\n";
         $log.="FILE变量：".json_encode($_FILES)."\r\n";
         $log.="REQUEST变量：".json_encode($_REQUEST)."\r\n";
+        $log.="TEMP目录下的文件：".json_encode(scandir(sys_get_temp_dir()))."\r\n";
         $log.=file_exists($_FILES["file"]["tmp_name"])?"文件存在\r\n":"文件不存在\r\n";
-        $log.=file_exists($_FILES["file"]["tmp_name"].$_REQUEST["chunk"])?"分片1存在\r\n":"分片1不存在\r\n";
+        $log.=file_exists($_FILES["file"]["tmp_name"]."1")?"分片1存在\r\n":"分片1不存在\r\n";
         if (move_uploaded_file($_FILES["file"]["tmp_name"],"data/update.xcpak")){
             $log.="上传成功\r\n";
             echo "successful";
@@ -27,7 +28,7 @@ class post
     }
     public static function applyUpdate(){
         xcpak::decode("./data/update.xcpak",PATH);
-        unlink(PATH."run.bat");
+//        unlink(PATH."run.bat");
         unlink(PATH."run.php");
     }
 //    private static function unzip($name,$out){
