1c7734f85e9b25fd0e51caceef301d1e37619aad
SCH <sch@xcgzs.cn>
2021-06-05 22:00:57 +0800

    更新get模块，优化代码

--- system/core/get.php
+++ system/core/get.php
@@ -2,20 +2,27 @@
 
 class get
 {
-    public static function index(){
-        loadHead(L["INDEX_TITLE"],["css"=>[["index","media=\"screen\""]]]);
+    public static function index()
+    {
+        loadHead(L["INDEX_TITLE"], ["css" => [["index", "media=\"screen\""]]]);
         loadBody("index");
     }
-    public static function update(){
-        loadHead("更新站点",["css"=>["zui.min","zui.uploader.min"],"js"=>["zui.uploader.min","webuploader.min"]]);
+
+    public static function update()
+    {
+        loadHead("更新站点", ["css" => ["zui.min", "zui.uploader.min", "update"], "js" => ["zui.uploader.min", "webuploader.min", "update"]]);
         loadBody("update");
     }
-    public static function classIntroduce(){
-        loadHead("胡杨班",["css"=>[["classIntroduce","media=\"screen\""]]]);
+
+    public static function classIntroduce()
+    {
+        loadHead("胡杨班", ["css" => [["classIntroduce", "media=\"screen\""]]]);
         loadBody("classIntroduce");
     }
-    public static function yule(){
-        loadHead("娱乐",["css"=>[["yule","media=\"screen\""]]]);
+
+    public static function yule()
+    {
+        loadHead("娱乐", ["css" => [["yule", "media=\"screen\""]]]);
         loadBody("yule");
     }
 }
