91143e45ab08a2f941e5698b46836c46be084ae7
SCH <sch@xcgzs.cn>
2021-06-19 17:44:52 +0800

    更新注释

--- system/core/cookie.php
+++ system/core/cookie.php
@@ -11,7 +11,8 @@ class cookie
     /**
      * @param string $name 数据名
      * @param string $value 数据内容
-     * @return cookie 返回cookie对象，可以用于连续操作
+     * @return cookie 返回cookie对象，用于连续操作
+     * 写入cookie数据到浏览器
      */
     static function set(string $name, string $value):cookie
     {
@@ -22,6 +23,7 @@ class cookie
     /**
      * @param string $name 数据名
      * @return mixed|null 返回cookie中的数据，若该数据不存在则返回null
+     * 从浏览器中读取cookie数据
      */
     static function get(string $name)
     {
@@ -32,8 +34,14 @@ class cookie
         }
     }
 
-    static function delete($name)
+    /**
+     * @param string $name 数据名
+     * @return cookie 返回cookie对象，用于连续操作
+     * 从浏览器中删除某个cookie数据
+     */
+    static function delete(string $name):cookie
     {
         setcookie($name, "", 0);
+        return new cookie();
     }
 }
