4068bda2054a47666cf1301bcb22a821bae2119c
SCH <sch@xcgzs.cn>
2021-06-19 19:23:59 +0800

    更新注释

--- system/core/dbTool.php
+++ system/core/dbTool.php
@@ -6,7 +6,10 @@ include "redirect.php";
  */
 class dbTool
 {
-
+    /**
+     * 获取当前数据库类型
+     * @return string 返回数据库类型：mysql/file
+     */
     private static function getDataBaseType(): string
     {//获取数据库类型
         if (isset($_GET["debug"]) || isset($_POST["debug"])) {
@@ -17,6 +20,10 @@ class dbTool
         return json_decode(file_get_contents(CONFIG_PATH . "db_config.json"), true)['type'];
     }
 
+    /**
+     * 连接数据库（MySQL）
+     * @return array 返回数组，array[0]为当前状态，fail为失败（array[1]为失败原因），success为成功(array[1]为MySQL对象)
+     */
     public static function connectDB(): array
     {//连接数据库(用于MySQL)
         $a = json_decode(file_get_contents(CONFIG_PATH . "db_config.json"), true);
@@ -28,6 +35,11 @@ class dbTool
         }
     }
 
+    /**
+     * 获取数据表的表头数据
+     * @param array $tableData 数据表
+     * @return array 返回标题数组
+     */
     private static function getTableHead(array $tableData): array
     {//获取表头行数据
         $b = array();
@@ -37,9 +49,15 @@ class dbTool
         return $b;
     }
 
+    /**
+     * 在数据库中创建数据表
+     * @param string $tableName 数据表名
+     * @param array $cols 数据表的列
+     * @return array 返回数组，详情请阅读对应数据库模块的文档
+     */
     public static function createTable(string $tableName, array $cols): array
     {//创建数据表
-        $result = "";
+        $result = [];
         switch (self::getDataBaseType()) {
             case "file":
                 $result = fileDBTool::createTable($tableName, $cols);
@@ -51,6 +69,13 @@ class dbTool
         return $result;
     }
 
+    /**
+     * 在数据表中插入一行数据
+     * @param string $tableName 数据表名
+     * @param array $keys 插入数据的列名
+     * @param array $values 插入的数据
+     * @return array|string 返回数组，详情请阅读对应数据库模块的文档
+     */
     public static function insert(string $tableName, array $keys, array $values): array
     {//插入数据
         $result = "";
@@ -65,6 +90,13 @@ class dbTool
         return $result;
     }
 
+    /**
+     * 从数据库中读取数据
+     * @param string $tableName 数据表名
+     * @param array $keys 需要读取的数据的键，全部数据使用["*"]
+     * @param array $where 查找条件
+     * @return array 返回数组，详情请阅读对应数据库模块的文档
+     */
     public static function select(string $tableName, array $keys, array $where): array
     {
         $result = "";
@@ -129,9 +161,16 @@ class dbTool
         return $result;
     }
 
-    public static function deleteRow(string $tableName, array $where)
+
+    /**
+     * 删除数据库中的一行
+     * @param string $tableName 数据表名
+     * @param array $where 删除条件
+     * @return array 返回数组，详情请阅读对应数据库模块的文档
+     */
+    public static function deleteRow(string $tableName, array $where):array
     {
-        $result = "";
+        $result = [];
         switch (self::getDataBaseType()) {
             case "file":
                 if (!count($where)) {
@@ -180,6 +219,11 @@ class dbTool
         return $result;
     }
 
+    /**
+     * 删除某个数据表
+     * @param string $tableName 数据表名
+     * @return array 返回数组，详情请阅读对应数据库模块的文档
+     */
     public static function deleteTable(string $tableName): array
     {
         $result = [];
@@ -213,6 +257,11 @@ class dbTool
         return $result;
     }
 
+    /**
+     * 判断数据表是否存在
+     * @param string $tableName 数据表名
+     * @return bool 返回一个布尔值，存在为true，不存在为false
+     */
     public static function tableExist(string $tableName): bool
     {
         switch (self::getDataBaseType()) {
@@ -228,9 +277,17 @@ class dbTool
         }
     }
 
+    /**
+     * 更新数据表中的某一行数据
+     * @param string $tableName 数据表名
+     * @param array $keys 数据列名
+     * @param array $values 数据内容
+     * @param array $where 查找条件
+     * @return array 返回数组，详情请阅读对应数据库模块的文档
+     */
     public static function update(string $tableName, array $keys, array $values, array $where): array
     {
-        $result = "";
+        $result = [];
         switch (self::getDataBaseType()) {
             case "file":
                 $a = json_decode(encryptTool::decode(file_get_contents(FILE_DB_PATH . $tableName . ".xcdb"), SECRET, true), true);
@@ -274,6 +331,10 @@ class dbTool
         return $result;
     }
 
+    /**
+     * 返回数据库类型的名称（显示内容与当前语言有关）
+     * @return string 当前数据库类型
+     */
     public static function DBType(): string
     {
         return self::getDataBaseType() == "mysql" ? LANGUAGE_DATA['DATABASE_TYPE_MYSQL'] : (self::getDataBaseType() == "file" ? LANGUAGE_DATA['DATABASE_TYPE_FILE'] : LANGUAGE_DATA['DATABASE_TYPE_UNKNOWN']);
