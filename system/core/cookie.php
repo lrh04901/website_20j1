f3ff461626d7e6d4b8538ab05f7a55be2e1e03ec
SCH <sch@xcgzs.cn>
2021-06-19 17:42:35 +0800

    更新注释

--- system/core/cookie.php
+++ system/core/cookie.php
@@ -9,21 +9,21 @@ include "redirect.php";
 class cookie
 {
     /**
-     * @param $name 数据名
-     * @param $value 数据内容
+     * @param string $name 数据名
+     * @param string $value 数据内容
      * @return cookie 返回cookie对象，可以用于连续操作
      */
-    static function set($name, $value):cookie
+    static function set(string $name, string $value):cookie
     {
         setcookie($name, $value);
         return new cookie();
     }
 
     /**
-     * @param $name 数据名
+     * @param string $name 数据名
      * @return mixed|null 返回cookie中的数据，若该数据不存在则返回null
      */
-    static function get($name)
+    static function get(string $name)
     {
         if (isset($_COOKIE[$name])) {
             return $_COOKIE[$name];
