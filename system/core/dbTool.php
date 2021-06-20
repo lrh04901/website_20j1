1cf0593d9fcd297e55321914116ef651f24100db
SCH <sch@xcgzs.cn>
2021-06-20 13:52:20 +0800

    更新数据库模块，优化代码调用

--- system/core/dbTool.php
+++ system/core/dbTool.php
@@ -40,7 +40,7 @@ class dbTool
      * @param array $tableData 数据表
      * @return array 返回标题数组
      */
-    private static function getTableHead(array $tableData): array
+    public static function getTableHead(array $tableData): array
     {//获取表头行数据
         $b = array();
         for ($i = 0; $i < count($tableData[0]); $i++) {
@@ -102,60 +102,10 @@ class dbTool
         $result = "";
         switch (self::getDataBaseType()) {
             case "file":
-                $a = json_decode(encryptTool::decode(file_get_contents(FILE_DB_PATH . $tableName . ".xcdb"), SECRET, true), true);
-                $data = array();
-                $b = self::getTableHead($a);
-                foreach ($a as $value) {
-                    if ($value[$b[array_keys($where)[0]]] == $where[array_keys($where)[0]]) {
-                        $t = array();
-                        foreach ($b as $item) {
-                            if ($keys[0] == "*" || in_array($item, $keys)) {
-                                $t[array_keys($b)[$item]] = $value[$item];
-                            }
-                        }
-                        $data[count($data)] = $t;
-                    }
-                }
-                $data = arrayTool::removeFirst($data);
-                $result = ["status" => "success", "data" => $data];
+                $result = fileDBTool::select($tableName,$keys,$where);
                 break;
             case "mysql":
-                $k = "";
-                if ($keys[0] == "*") {
-                    $k = $keys[0];
-                } else {
-                    foreach ($keys as $item) {
-                        $k .= $item . ",";
-                    }
-                    $k = substr($k, 0, strlen($k) - 1);
-                }
-                $w = "";
-                if (count($where)) {
-                    $w = "WHERE ";
-                    for ($i = 0; $i < count($where); $i++) {
-                        $w .= array_keys($where)[$i] . "='" . $where[array_keys($where)[$i]] . "',";
-                    }
-                    $w = substr($w, 0, strlen($w) - 1);
-                }
-                $a = "SELECT $k FROM $tableName $w;";
-                $conn = self::connectDB();
-                if ($conn["status"] === "success") {
-                    $r = mysqli_query($conn["conn"], $a);
-                    if (!$r) {
-                        $result = ["status" => "fail", "reason" => mysqli_error($conn["conn"])];
-                    }
-//                    print_r($r);
-//                    die();
-                    $data = array();
-                    while ($row = mysqli_fetch_assoc($r)) {
-                        unset($row['id']);
-                        $data[count($data)] = $row;
-                    }
-                    $result = ["status" => "success", "data" => $data];
-//                    print_r($result);
-                } else {
-                    $result = ["status" => "fail", "reason" => $conn["reason"]];
-                }
+                $result = mysqlTool::select($tableName,$keys,$where);
                 break;
         }
         return $result;
@@ -173,47 +123,10 @@ class dbTool
         $result = [];
         switch (self::getDataBaseType()) {
             case "file":
-                if (!count($where)) {
-                    $result = ["status" => "fail", "reason" => "where参数不能为空数组，若要删除所有数据请指定where为[*]"];
-                    break;
-                }
-                $a = json_decode(encryptTool::decode(file_get_contents(FILE_DB_PATH . $tableName . ".xcdb"), SECRET, true), true);
-                $h = self::getTableHead($a);
-                $c = array();
-                for ($i = 1; $i < count($a); $i++) {//遍历总数据的每一行
-                    for ($j = 0; $j < count($where); $j++) {//遍历条件的数据
-                        if ($a[$i][$h[array_keys($where)[$j]]] == $where[array_keys($where)[$j]]) {
-                            $c[count($c)] = $a[$i];
-                        }
-                    }
-                }
-                foreach ($c as $item) {
-                    while (in_array($item, $a)) {
-                        $a = arrayTool::remove($a, $item);
-                    }
-                }
-                file_put_contents(FILE_DB_PATH . $tableName . ".xcdb", encryptTool::encode(json_encode($a), SECRET, true));
-                $result = ["status" => "success"];
+                $result = fileDBTool::deleteRow($tableName,$where);
                 break;
             case "mysql":
-                $a = "DELETE FROM $tableName";
-                $c = self::connectDB();
-                if ($c["status"] === "success") {
-                    if (count($where)) {
-                        $a .= " WHERE ";
-                        for ($i = 0; $i < count($where); $i++) {
-                            $a .= array_keys($where)[$i] . "='" . $where[array_keys($where)[$i]] . "',";
-                        }
-                        $a = substr($a, 0, strlen($a) - 1);
-                    }
-                    if (mysqli_query($c["conn"], $a)) {
-                        $result = ["status" => "success"];
-                    } else {
-                        $result = ["status" => "fail", "reason" => mysqli_error($c["conn"])];
-                    }
-                } else {
-                    $result = ["status" => "fail", "reason" => $c["reason"]];
-                }
+                $result = mysqlTool::deleteRow($tableName,$where);
                 break;
         }
         return $result;
@@ -229,29 +142,10 @@ class dbTool
         $result = [];
         switch (self::getDataBaseType()) {
             case "file":
-                if (file_exists(FILE_DB_PATH . $tableName . ".xcdb")) {
-                    if (unlink(FILE_DB_PATH . $tableName . ".xcdb")) {
-                        $result = ["status" => "success"];
-                    } else {
-                        $result = ["status" => "fail", "reason" => ""];
-                    }
-                } else {
-                    $result = ["status" => "fail", "reason" => "数据表不存在"];
-                }
+                $result = fileDBTool::deleteTable($tableName);
                 break;
             case "mysql":
-                $a = "DROP TABLE $tableName";
-                $conn = self::connectDB();
-                if ($conn["status"] === "success") {
-                    $r = mysqli_query($conn["conn"], $a);
-                    if (!$r) {
-                        $result = ["status" => "fail", "reason" => mysqli_error($conn["conn"])];
-                        return $result;
-                    }
-                    $result = ["status" => "success"];
-                } else {
-                    $result = ["status" => "fail", "reason" => $conn["reason"]];
-                }
+                $result = mysqlTool::deleteTable($tableName);
                 break;
         }
         return $result;
@@ -264,17 +158,16 @@ class dbTool
      */
     public static function tableExist(string $tableName): bool
     {
+        $result = [];
         switch (self::getDataBaseType()) {
             case "file":
-                return file_exists(FILE_DB_PATH . $tableName . ".xcdb");
+                $result = fileDBTool::tableExist($tableName);
+                break;
             case "mysql":
-                $conn = self::connectDB();
-                if ($conn["status"] === "success") {
-                    return mysqli_query($conn["conn"], "SELECT * FROM " . $tableName) ? true : false;
-                } else {
-                    return false;
-                }
+                $result = mysqlTool::tableExist($tableName);
+                break;
         }
+        return $result;
     }
 
     /**
@@ -290,42 +183,10 @@ class dbTool
         $result = [];
         switch (self::getDataBaseType()) {
             case "file":
-                $a = json_decode(encryptTool::decode(file_get_contents(FILE_DB_PATH . $tableName . ".xcdb"), SECRET, true), true);
-                $h = self::getTableHead($a);
-                for ($i = 1; $i < count($a); $i++) {
-                    if ($a[$i][$h[array_keys($where)[0]]] == $where[array_keys($where)[0]]) {
-                        for ($j = 0; $j < count($keys); $j++) {
-                            $a[$i][$h[$keys[$j]]] = $values[$j];
-                        }
-                    }
-                }
-                file_put_contents(FILE_DB_PATH . $tableName . ".xcdb", encryptTool::encode(json_encode($a), SECRET, true));
-                $result = ["status" => "success"];
+                $result = fileDBTool::update($tableName,$keys,$values,$where);
                 break;
             case "mysql":
-                $a = "UPDATE " . $tableName . " SET ";
-                for ($i = 0; $i < count($keys); $i++) {
-                    $a .= $keys[$i] . "='" . $values[$i] . "',";
-                }
-                $a = substr($a, 0, strlen($a) - 1);
-                if (count($where)) {
-                    $a .= " WHERE ";
-                    for ($i = 0; $i < count($where); $i++) {
-                        $a .= array_keys($where)[$i] . "='" . $where[array_keys($where)[$i]] . "',";
-                    }
-                    $a = substr($a, 0, strlen($a) - 1);
-                }
-                $c = self::connectDB();
-                if ($c["status"] === "success") {
-                    $r = mysqli_query($c["conn"], $a);
-                    if (!$r) {
-                        $result = ["status" => "fail", "reason" => mysqli_error($c["conn"])];
-                    } else {
-                        $result = ["status" => "success"];
-                    }
-                } else {
-                    $result = ["status" => "fail", "reason" => $c["reason"]];
-                }
+                $result = mysqlTool::update($tableName,$keys,$values,$where);
                 break;
         }
         return $result;
@@ -337,6 +198,6 @@ class dbTool
      */
     public static function DBType(): string
     {
-        return self::getDataBaseType() == "mysql" ? LANGUAGE_DATA['DATABASE_TYPE_MYSQL'] : (self::getDataBaseType() == "file" ? LANGUAGE_DATA['DATABASE_TYPE_FILE'] : LANGUAGE_DATA['DATABASE_TYPE_UNKNOWN']);
+        return self::getDataBaseType() == "mysql" ? L['DATABASE_TYPE_MYSQL'] : (self::getDataBaseType() == "file" ? L['DATABASE_TYPE_FILE'] : L['DATABASE_TYPE_UNKNOWN']);
     }
 }
