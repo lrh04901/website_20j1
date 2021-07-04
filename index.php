b6ea209cf5ddfc74849d94f5f407991c734ae654
SCH <sch@xcgzs.cn>
2021-07-04 20:12:11 +0800

    优化启动代码

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php
 if (file_exists("debug")){
     include("system/core/dev/core.php");
-    core::initialize();
-    core::runWeb();
 }else{
     include("system/core/core.phar");
     include("phar://core.phar/core.php");
-    core::initialize();
-    core::runWeb();
 }
+core::initialize();
+core::runWeb();
 //if (file_exists("system/core/core.php")){
 ////    include("system/core/core.php");
 ////    core::initialize();
