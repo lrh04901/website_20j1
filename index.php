59a69f55842c7e6cf8c8e8e23e383213b0d1a41d
SCH <sch@xcgzs.cn>
2021-07-04 19:15:13 +0800

    修改启动代码

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
-if (file_exists("system/core/core.php")){
-    include("system/core/core.php");
+if (file_exists("debug")){
+    include("system/core/dev/core.php");
     core::initialize();
     core::runWeb();
 }else{
-    die("核心代码不存在");
+    include("system/core/core.phar");
+    include("phar://core.phar/core.php");
+    core::initialize();
+    core::runWeb();
 }
+//if (file_exists("system/core/core.php")){
+////    include("system/core/core.php");
+////    core::initialize();
+////    core::runWeb();
+//}else{
+//    die("核心代码不存在");
+//}
