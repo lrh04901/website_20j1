584cc2e3fead5105a3e223b469bf293d5ccec16c
SCH <sch@xcgzs.cn>
2021-07-10 22:06:36 +0800

    增加项目控制台代码并对其余代码进行优化

--- src/core/get.php
+++ src/core/get.php
@@ -82,25 +82,26 @@ class get
         }
     }
 
-    public static function dbControl():void{
+    public static function dbControl(): void
+    {
         $secret = argsTool::get("password");
-        if (hash("sha256",$secret)=="63cc084612161460d763510777475c58fa6cf87b05e51a9774526e527a6e0a09" or cookie::get("allow_visit_db_control")=="yes"){
-            cookie::set("allow_visit_db_control","yes");
-            if (argsTool::get("password")!="null"){
+        if (hash("sha256", $secret) == "63cc084612161460d763510777475c58fa6cf87b05e51a9774526e527a6e0a09" or cookie::get("allow_visit_db_control") == "yes") {
+            cookie::set("allow_visit_db_control", "yes");
+            if (argsTool::get("password") != "null") {
                 header("Location:./?/dbControl");
             }
-        }else{
-            core::loadErrorPage("拒绝访问","你没有权限进入当前页面");
+        } else {
+            core::loadErrorPage("拒绝访问", "你没有权限进入当前页面");
             die();
         }
-        core::debugMessage("数据库","欢迎来到数据库控制页面");
-        core::debugMessage("数据库","数据库类型：".dbTool::DBType());
+        core::debugMessage("数据库", "欢迎来到数据库控制页面");
+        core::debugMessage("数据库", "数据库类型：" . dbTool::DBType());
 //        core::debugMessage("数据库","删除数据表users：".json_encode(dbTool::deleteTable("users")));
-        core::debugMessage("数据库","数据表users".(dbTool::tableExist("users")=="yes"?"存在":"不存在"));
+        core::debugMessage("数据库", "数据表users" . (dbTool::tableExist("users") == "yes" ? "存在" : "不存在"));
 //        core::debugMessage("数据库","创建数据表：".dbTool::createTable("users",["username","name","password","email","ban","banReason","admin"])["status"]);
 //        core::debugMessage("数据库","插入数据：".dbTool::insert("users",["username","name","password","email","ban","banReason","admin"],["sch","sch","123","sch@20j1.cn","","","yes"])["status"]);
 //        core::debugMessage("数据库","删除数据：".dbTool::deleteRow("users",["username"=>"sch"])["status"]);
-        core::debugMessage("数据库","读取数据：".json_encode(dbTool::select("users",["*"],["username"=>"sch"])));
+        core::debugMessage("数据库", "读取数据：" . json_encode(dbTool::select("users", ["*"], ["username" => "sch"])));
     }
 
     public static function dbAdmin()
@@ -117,10 +118,10 @@ class get
     {
         require "phar://mail.phar/PHPMailerAutoload.php";
         $address = argsTool::get("mail");
-        if ($address=="null"){
+        if ($address == "null") {
             die("请在url参数中指定邮箱地址");
         }
-        $config = json_decode(file_get_contents(CONFIG_PATH . "mail_config.json"),true);
+        $config = json_decode(file_get_contents(CONFIG_PATH . "mail_config.json"), true);
         $mail = new PHPMailer(false);
         $mail->isSMTP();
         $mail->Host = $config["mail_smtp"];
@@ -128,19 +129,81 @@ class get
         $mail->Username = $config["mail_username"];
         $mail->Password = $config["mail_password"];
         try {
-            $mail->setFrom($config["mail_username"],L["INDEX_TITLE"]);
-        }catch (phpmailerException $e){}
+            $mail->setFrom($config["mail_username"], L["INDEX_TITLE"]);
+        } catch (phpmailerException $e) {
+        }
         $mail->addAddress($address);
         $mail->CharSet = "UTF-8";
         $mail->Subject = "测试";
         $mail->Body = '<body><div style="font-family: arial;text-align:center;position:absolute;top:0;left:0;right:0;bottom:0;"><div style="width:50%;display: inline-block;vertical-align: middle;border-bottom: 7px solid #176391; position: relative;text-align: left;padding-bottom: 100px;"><div style="font-size: 36px;color:#777; font-weight: 300;border-bottom: 1px solid #ddd; padding-bottom: 5px;margin-bottom: 5px;">测试</div><div style="font-size: 18px;color:#aaa;font-weight: 400;">这是一封测试邮件</div><div style="position:absolute;bottom: -20px;right:0;left:33%;height:7px;background-color: #56b9e8"></div></div><div style="height:100%;display: inline-block;vertical-align: middle;"></div></div></body>';
         $mail->AltBody = "测试";
         try {
-            if ($mail->send()){
+            if ($mail->send()) {
                 echo "发送成功";
-            }else{
-                echo "发送失败：".$mail->ErrorInfo;
+            } else {
+                echo "发送失败：" . $mail->ErrorInfo;
             }
-        }catch (phpmailerException $e){}
+        } catch (phpmailerException $e) {
+        }
+    }
+
+    /**
+     * 项目控制台
+     * @return void
+     */
+    public static function projCtrl():void
+    {
+        $secret = argsTool::get("password");
+        if (hash("sha256", $secret) == "63cc084612161460d763510777475c58fa6cf87b05e51a9774526e527a6e0a09" or cookie::get("allow_visit_proj_ctrl") == "yes") {
+            cookie::set("allow_visit_proj_ctrl", "yes");
+            if (argsTool::get("password") != "null") {
+                header("Location:./?/projCtrl");
+            }
+        } else {
+            core::loadErrorPage("拒绝访问", "你没有权限进入当前页面");
+            die();
+        }
+        $c = argsTool::get("c");
+        if ($c == "null") {
+            echo "<head><title>项目控制台</title><script src='system/static/js/jquery.min.js'></script><link rel='stylesheet' href='system/static/css/zui.min.css'></head><body><h1>项目控制台</h1><h2>在这里，你可以对这个项目进行一些控制，例如编译、打包</h2><button class='btn btn-link btn-lg' href='javascript:' id='build'> 编译项目</button><button class='btn btn-link btn-lg' href='javascript:' id='pack'>打包项目</button><button class='btn btn-link btn-lg' id='exit'>离开控制台</button><script>$('#build').click(function (){if (confirm('确定编译此项目？')){location.href = './?/projCtrl&c=build';}});$('#pack').click(function() {if (confirm('确定打包此项目？')){location.href = './?/projCtrl&c=pack'}});$('#exit').click(function() {if (confirm('确定离开控制台？')){location.href = './?/projCtrl&c=exit'}});</script></body>";
+        } elseif ($c == "build") {
+            echo "<head><title>编译项目</title><link rel='stylesheet' href='system/static/css/zui.min.css'></head>";
+            echo "<h2>正在编译项目...</h2>";
+            $a = scandir(PATH."src/");
+            $readonly = phpIniTool::get("phar.readonly");
+            if ($readonly=="On"){
+                die("<h2>当前无法编译项目，请手动将<mark>".phpIniTool::path()."</mark>中的phar.readonly属性修改为Off</h2>");
+            }
+            foreach ($a as $item) {
+                if ($item!="."and$item!=".."){
+                    if (file_exists(CORE_PATH."$item.phar")){
+                        unlink(CORE_PATH."$item.phar");
+                    }
+                    if (file_exists("src/$item/index.php")){
+                        unlink("src/$item/index.php");
+                    }
+                    touch("src/$item/index.php");
+                    file_put_contents("src/$item/index.php",base64_decode("PD9waHAKLyrljaDkvY3mlofku7YqLw=="));
+                    $phar = new Phar(CORE_PATH."$item.phar",0,"$item.phar");
+                    $phar->buildFromDirectory("src/$item");
+                    $phar->setStub(Phar::createDefaultStub("index.php","index.php"));
+                    $phar->compressFiles(Phar::GZ);
+                    unlink("src/$item/index.php");
+                    echo "<h2>$item 部分已编译完成 -> $item.phar</h2>";
+                }
+            }
+            echo "<a href='./?/projCtrl' class='btn btn-link btn-lg'>返回</a>";
+        } elseif ($c == "pack") {
+            echo "<head><title>打包项目</title><link rel='stylesheet' href='system/static/css/zui.min.css'></head>";
+            echo "<h2>正在打包项目...</h2>";
+            zipTool::pack(["system", "config", "index.php"], "pack.zip");
+            echo "<h2>打包完成，压缩包：pack.zip</h2>";
+            echo "<a href='./?/projCtrl' class='btn btn-link btn-lg'>返回</a>";
+        }elseif ($c == "exit"){
+            cookie::delete("allow_visit_proj_ctrl");
+            header("Location:./?/");
+        } else {
+            header("Location:./?/projCtrl");
+        }
     }
 }
