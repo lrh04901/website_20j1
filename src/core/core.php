3bcc39656ab08eb90252411085024f346dd1b36e
SCH <sch@xcgzs.cn>
2021-07-12 22:24:31 +0800

    优化https跳转的功能

--- src/core/core.php
+++ src/core/core.php
@@ -12,14 +12,14 @@ class core
     public static function initialize(): void
     {
 //        echo "[Debug Info]<br>";
+        include("./system/core/core.phar");
+        self::loadComponent("define");//加载定义模块
+        define::run();
         if (json_decode(file_get_contents(CONFIG_PATH."sys_config.json"))["enable_https"]){
             if ($_SERVER["SERVER_PORT"]=="80"){
                 header("Location:https://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"]);
             }
         }
-        include("./system/core/core.phar");
-        self::loadComponent("define");//加载定义模块
-        define::run();
         self::loadComponent("cookie");//加载cookie模块
         self::loadComponent("language");//加载语言模块
         language::run();
