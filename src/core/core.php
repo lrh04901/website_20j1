2c12f0ea178b81cbe11816f2e56a377a379c254e
SCH <sch@xcgzs.cn>
2021-07-12 22:23:46 +0800

    增加https跳转功能

--- src/core/core.php
+++ src/core/core.php
@@ -12,6 +12,11 @@ class core
     public static function initialize(): void
     {
 //        echo "[Debug Info]<br>";
+        if (json_decode(file_get_contents(CONFIG_PATH."sys_config.json"))["enable_https"]){
+            if ($_SERVER["SERVER_PORT"]=="80"){
+                header("Location:https://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"]);
+            }
+        }
         include("./system/core/core.phar");
         self::loadComponent("define");//加载定义模块
         define::run();
