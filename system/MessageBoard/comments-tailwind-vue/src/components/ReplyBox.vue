012802cac0a9a91df47af8fa4afce201801e3e46
lrhhz <2372724252@qq.com>
2021-08-01 16:49:35 +0800

    回复显示区域组件

---
+++ system/MessageBoard/comments-tailwind-vue/src/components/ReplyBox.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="pl-8 border-l-2 border-gray-200">
+    <!--回复内容-->
+    <slot />
+  </div>
+  <button class="pt-4 pb-10 text-blue-600">回复</button>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
