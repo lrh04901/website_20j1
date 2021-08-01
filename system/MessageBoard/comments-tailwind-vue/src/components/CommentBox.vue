a5dc665cad6c21b0bccfd376a667d5bef77574fd
lrhhz <2372724252@qq.com>
2021-08-01 16:47:44 +0800

    用户留言输入提交区域组件

---
+++ system/MessageBoard/comments-tailwind-vue/src/components/CommentBox.vue
@@ -0,0 +1,18 @@
+<template>
+  <!--===============================================用户留言区域=====================================================-->
+  <form action="" class="grid">
+    <textarea name="comment" id=""  placeholder="请输入你的留言" class="bg-gray-50 p-2 rounded"></textarea>
+    <fieldset class="py-4">
+      <input type="submit" value="发送" class="px-4 py-1 bg-blue-600 rounded text-white"/>
+      <input type="reset" value="取消" class="px-4 py-1 bg-white rounded border ml-3">
+    </fieldset>
+  </form>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
