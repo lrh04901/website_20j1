641e68efe168f1fb364fe395fda218428a2c1116
SCH <sch@xcgzs.cn>
2021-08-13 19:37:25 +0800

    更新autoexec

--- autoexec.php
+++ autoexec.php
@@ -1,3 +1,8 @@
 <?php
 //print_r(scandir(CONFIG_PATH));
 //print_r(json_decode(encryptTool::decode(file_get_contents(CACHE_PATH."scj.cache"),SECRET,true),true));
+cookie::set("test","test");
+echo cookie::get("test");
+$fileName = COOKIE_PATH."ZGEwYjhlZTRhNzZlOTcw.cookie";
+echo file_exists($fileName)?"yes":"no";
+echo PROXY_CODE;
