6ad387fb16fff679a8cbcc5ab03ce803c498f8d3
lrhhz <2372724252@qq.com>
2021-08-24 14:39:51 +0800

    开启jit模式，加快vite项目编译速度

--- system/MessageBoard/comments-tailwind-vue/tailwind.config.js
+++ system/MessageBoard/comments-tailwind-vue/tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: ['./index.html'],
+  mode: "jit",//开启jit模式
+  purge: ['./index.html', './src/**/*.{vue, js}'],//处理css文件,使jit匹配src文件夹下的vue和js文件
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
